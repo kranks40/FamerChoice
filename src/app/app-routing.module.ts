c53b94ade1b60d4fc96f5d54c16bbb6474f9499f
kranks40 <eng_peters20.yahoo.com>
2020-06-29 00:15:07 -0400

    Initial app stage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,33 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    {
+   path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: 'welcome',
+   loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)},
+ // {
+   // path: 'login',
+   // loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+ // {
+   // path: 'signup',
+   // loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)},
+ // {
+   // path: 'home',
+    //loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+ // {
+   // path: 'category',
+   // loadChildren: () => import('./pages/category/category.module').then( m => m.CategoryPageModule)},
+ // {
+   // path: 'favorite',
+   // loadChildren: () => import('./pages/favorite/favorite.module').then( m => m.FavoritePageModule)},
+ // {
+   // path: 'feed',
+  //  loadChildren: () => import('./pages/feed/feed.module').then( m => m.FeedPageModule)},
+ // {
+   // path: 'settings',
+   // loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)}
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home',
+        loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'category',
+        loadChildren: () => import('../pages/category/category.module').then(m => m.CategoryPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'favorite',
+        loadChildren: () => import('../pages/favorite/favorite.module').then(m => m.FavoritePageModule)
+      },
+      {
+        path: 'feed',
+        loadChildren: () => import('../pages/feed/feed.module').then(m => m.FeedPageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../pages/settings/settings.module').then(m => m.SettingsPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
